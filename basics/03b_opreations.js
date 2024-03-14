e3ae6f1fb5f5a222936a40f84a68281935b27871
yash <khatiyash010703@gmail.com>
2024-03-14 20:54:18 +0530

    opreations

---
+++ basics/03b_opreations.js
@@ -0,0 +1,44 @@
+/*
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+let str1 = "hello"
+let str2 = " Yash"
+
+let concatString = str1+str2
+console.log(concatString)
+
+
+console.log(1 + "2") // 12 (string)
+console.log("1" + 2) // 12 (string)
+
+console.log(1 + 2 + "2") // 32 ( string )
+console.log("1" + 2 + 2) // 122 ( string )
+*/
+
+/*
+
+console.log(true) // true
+console.log(+true) // 1
+console.log(-true) // -1
+console.log(+"") // 0
+
+*/
+
+/*
+
+let num1 , num2 , num3
+num1 = num2 = num3 = 2+2
+console.table([num1, num2 , num3])
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+console.log(++gameCounter)
+console.log(gameCounter)
+console.log(gameCounter++)
+console.log(gameCounter)
+
+*/
+
