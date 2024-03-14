0445da3e4ab0c8928d0a614db2fc85bedc74baf5
yash <khatiyash010703@gmail.com>
2024-03-14 20:38:17 +0530

    conversion Operations

---
+++ basics/03_conversionOpreation.js
@@ -0,0 +1,124 @@
+
+let score = "33"
+
+console.log(typeof score)
+
+/*
+    "33" -> 33
+    "33abc" -> NaN
+    null -> 0
+    true/false -> 1/0
+    undefined -> NaN
+*/
+
+/*
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let score2 = "33abc"
+let valueInNumber2 = Number(score2)
+console.log(typeof valueInNumber2)
+console.log(valueInNumber2)
+
+
+let score3 = null
+let valueInNumber3 = Number(score3)
+console.log(typeof valueInNumber3)
+console.log(valueInNumber3)
+
+
+let score4 = undefined
+let valueInNumber4 = Number(score4)
+console.log(typeof valueInNumber4)
+console.log(valueInNumber4)
+
+
+let score5 = true
+let valueInNumber5 = Number(score5)
+console.log(typeof valueInNumber5)
+console.log(valueInNumber5)
+
+*/
+
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+/*
+    1/0/-1 = true/false/true
+    "" -> false
+    "asas" -> true
+    null -> false
+    undefined -> false
+*/
+/*
+isLoggedIn = 0
+booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+isLoggedIn = -1
+booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+isLoggedIn = ""
+booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+isLoggedIn = "0"
+booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+isLoggedIn = undefined
+booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+isLoggedIn = null
+booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+*/
+
+
+
+/*
+    33 -> "33"
+    null -> "null"
+    undefined -> "undefined"
+    false -> "false"
+
+*/
+
+
+/*
+let checkString = 33
+let setToString = String(checkString)
+
+console.log(typeof setToString)
+console.log(setToString)
+
+checkString = null
+setToString = String(checkString)
+console.log(typeof setToString)
+console.log(setToString)
+
+
+checkString = undefined
+setToString = String(checkString)
+console.log(typeof setToString)
+console.log(setToString)
+
+checkString = false
+setToString = String(checkString)
+console.log(typeof setToString)
+console.log(setToString)
+
+
+*/
