a09b716d2e52211e2ad5bc1c01b37e3c52688722
yash <khatiyash010703@gmail.com>
2024-03-15 23:25:00 +0530

    function basics passing parameters

--- functions/01_functions.js
+++ functions/01_functions.js
@@ -10,7 +10,7 @@ const result = addTwoNumbers(3,5)
 console.log("Result : ",result) // undefined
 
 */
-
+/*
 function addTwoNumbers(num1, num2)
 {
     let result = num1 + num2
@@ -20,8 +20,8 @@ function addTwoNumbers(num1, num2)
 
 const result = addTwoNumbers(3,5)
 // console.log("Result : ",result) 
-
-
+*/
+/*
 
 function loginUserMessage(username) {
     if(username === undefined){
@@ -44,3 +44,46 @@ console.log(loginUserMessage())
 
 console.log(loginUserMessage2("Yash"))
 console.log(loginUserMessage2())
+
+*/
+
+/*
+    Function with any numbers of arguments
+
+*/
+
+function calculateCarPrice(...num1){
+    return num1
+}
+
+console.log(calculateCarPrice(200,400,500)) // [200,400,500]
+
+function calculateCarPrice2(val1,val2,...num1){
+    return num1
+}
+
+console.log(calculateCarPrice2(200,400,500,600)) 
+
+const user = {
+    username : "Yash",
+    price : 199,
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price} `)
+}
+
+handleObject(user)
+
+handleObject({
+    username : "Sam",
+    price : 999,
+})
+
+
+const myNewArray = [100,400,300,900]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray))
