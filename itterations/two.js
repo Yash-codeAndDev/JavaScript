6b3966e0e359fa2799fa4367df04c700152bf616
yash <khatiyash010703@gmail.com>
2024-03-16 12:10:07 +0530

    for of loop and Map

---
+++ itterations/two.js
@@ -0,0 +1,56 @@
+// for of loop
+
+const arr = [1, 2, 3, 4, 5]
+/*
+for (const val of arr) {
+    console.log(val)
+}
+*/
+
+const greetings = "Hello How are You"
+/*
+for (const char of greetings) {
+    console.log(char+" ")
+}
+*/
+
+/*
+// Maps
+
+const map = new Map()
+map.set('IN' , "India")
+// map.set('IN' , "IndiaA")  // overrites IN key value pair
+map.set('USA' , "United States of America")
+map.set('FR' , "France")
+
+console.log(map)
+*/
+
+/*
+// print key value in form of array
+for (const key of map) {
+    console.log(key)
+}
+
+for (const [key , value] of map) {
+    console.log(key, ': - ', value)
+}
+*/
+
+const myObject = {
+    game1 : "NFS",
+    game2 : "Spiderman"
+}
+
+/*
+
+for (const [key,value] of myObject) {
+    console.log(ele)
+}
+
+*/
+
+
+
+
+
